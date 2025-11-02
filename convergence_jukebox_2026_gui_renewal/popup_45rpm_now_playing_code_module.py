a48051db85204347a92bba10621cf254d3b3e330
bradfortner <bradfortner@gmail.com>
2025-11-02 15:15:40 -0500

    Change popup window x position to 825

--- convergence_jukebox_2026_gui_renewal/popup_45rpm_now_playing_code_module.py
+++ convergence_jukebox_2026_gui_renewal/popup_45rpm_now_playing_code_module.py
@@ -298,7 +298,7 @@ def display_45rpm_now_playing_popup(MusicMasterSongList, counter, jukebox_select
             layout,
             no_titlebar=True,
             keep_on_top=True,
-            location=(820, 280),
+            location=(825, 280),
             background_color='black',
             margins=(0, 0),
             element_padding=(0, 0),
